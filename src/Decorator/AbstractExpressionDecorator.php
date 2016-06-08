5c1160626868655fcf0b4178782bca2466926c65
allflame <taras.p.gyrnik@gmail.com>
2016-06-08 13:24:34 +0300

    Initial commit

---
+++ src/Decorator/AbstractExpressionDecorator.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 4/1/16
+ * Time: 10:26 AM
+ */
+
+namespace Vain\Expression\Decorator;
+
+use Vain\Expression\Serializer\SerializerInterface;
+use Vain\Expression\ExpressionInterface;
+use Vain\Expression\Visitor\VisitorInterface;
+
+abstract class AbstractExpressionDecorator implements ExpressionInterface
+{
+    private $expression;
+
+    /**
+     * AbstractVainExpressionDecorator constructor.
+     * @param ExpressionInterface $expression
+     */
+    public function __construct(ExpressionInterface $expression)
+    {
+        $this->expression = $expression;
+    }
+
+    /**
+     * @return ExpressionInterface
+     */
+    public function getExpression()
+    {
+        return $this->expression;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function accept(VisitorInterface $visitor)
+    {
+        return $this->expression->accept($visitor);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString()
+    {
+        return $this->expression->__toString();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function serialize(SerializerInterface $serializer)
+    {
+        return $this->expression->serialize($serializer);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function unserialize(SerializerInterface $serializer, array $serializedData)
+    {
+        return $this->expression->unserialize($serializer, $serializedData);
+    }
+}
---
+++ src/Exception/UnknownExpressionException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 4/4/16
+ * Time: 11:13 AM
+ */
+
+namespace Vain\Expression\Exception;
+
+use Vain\Expression\Factory\ExpressionFactoryInterface;
+
+class UnknownExpressionException extends ExpressionFactoryException
+{
+    /**
+     * VainExpressionFactoryUnklnownShortcutException constructor.
+     * @param ExpressionFactoryInterface $expressionFactory
+     * @param string $shortcut
+     */
+    public function __construct(
+        ExpressionFactoryInterface $expressionFactory,
+        $shortcut
+    ) {
+        parent::__construct($expressionFactory, sprintf('Cannot create expression by unknown shortcut %s', $shortcut), 0, null);
+    }
+}
