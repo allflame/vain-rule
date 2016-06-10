36eb769909adfa2a3440cf69491880ad272b80d0
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-06-10 13:16:58 +0300

    Added result factory

---
+++ src/Result/Factory/ResultFactoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 6/10/16
+ * Time: 12:12 PM
+ */
+
+namespace Vain\Rule\Result\Factory;
+
+
+interface ResultFactoryInterface
+{
+
+}
