18e76c5f8a126d007101b3455b330c625f444d9a
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-06-15 12:32:37 +0300

    Added rule result

---
+++ src/Result/RuleResultInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 6/15/16
+ * Time: 12:29 PM
+ */
+
+namespace Vain\Rule\Result;
+
+use Vain\Expression\Boolean\Result\BooleanResultInterface;
+
+interface RuleResultInterface extends BooleanResultInterface
+{
+
+}
