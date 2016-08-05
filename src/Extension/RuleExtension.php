7540789a913db08432335c3bb6980a5d116fd238
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:29:05 +0300

    Added di.yml

---
+++ src/Extension/RuleExtension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-rule
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-rule
+ */
+namespace Vain\Rule\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class RuleExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class RuleExtension extends AbstractExtension
+{
+}
